7345e992554b59f5a44f374a742f6c2f2beee78a
sony9997 <sony9997@sina.com>
2025-01-17 17:55:27 +0800

    解决跨域问题

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    cors: {
+      origin: '*',
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization']
+    }
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
